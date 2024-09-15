9651ca4ceb92ca7fdad83225fc3f73cac02f7212
EdiZhang123 <edizhang05@gmail.com>
2024-09-15 16:09:35 -0700

    Update Home Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages";
 import About from "./pages/about";
 import Blog from "./pages/blog";
 
@@ -13,7 +12,6 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
